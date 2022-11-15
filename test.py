d9c28ef1277442002b1b12d5008ee65eb3516fd7
tien.nq <tien.nq@teko.vn>
2022-11-15 14:00:09 +0700

    fix conflict

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 print('commit - 1')
-print('commit of feature 1 time 13:50')
+print('commit of feature 2 time 13:53')
 print(knights.items())
